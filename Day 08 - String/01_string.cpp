f82d59d99cac6951e22165c25aeb556d36db6e1b
Ganpat <coolganpat41@gmail.com>
2024-02-20 20:32:28 +0530

    1st string program

---
+++ Day 08 - String/01_string.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "---Learning Strings---" << endl;
+    string name;
+    cout << "Enter a word: " << endl;
+    cin >> name; // it only takes a single word
+    cout << "You entered: " << name << endl;
+
+    return 0;
+}
+
+/*Output
+---Learning Strings---
+Enter a word:
+Ganpat
+You entered: Ganpat
+*/
