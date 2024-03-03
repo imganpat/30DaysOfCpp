b8988355f4d0ce0dbc8771ce9aff884ce6a3671e
Ganpat <coolganpat41@gmail.com>
2024-03-03 17:26:45 +0530

    program to take 2 objects as parameters and return a 3rd object

---
+++ Day 17 - Classes &  Objects/04_object_parameter_return.cpp
@@ -0,0 +1,62 @@
+git #include <iostream>
+using namespace std;
+
+class Distance
+{
+public:
+    float feet, inch;
+
+public:
+    void acceptDistance()
+    {
+        cout << "Enter feet ";
+        cin >> feet;
+
+        cout << "Enter inch ";
+        cin >> inch;
+    }
+
+    void displayDistance()
+    {
+        cout << "Feet: " << feet << endl;
+        cout << "Inch: " << inch << endl;
+    }
+
+    Distance totalDistance(Distance, Distance);
+};
+
+Distance totalDistance(Distance a, Distance b)
+{
+    Distance res;
+    res.feet = a.feet + b.feet;
+    res.inch = a.inch + b.inch;
+    return res;
+}
+
+int main()
+{
+    Distance d1, d2, d3;
+
+    d1.acceptDistance();
+    d2.acceptDistance();
+
+    d1.displayDistance();
+    d2.displayDistance();
+
+    d3 = totalDistance(d1, d2);
+    d3.displayDistance();
+
+    return 0;
+}
+
+// output
+// Enter feet 5
+// Enter inch 3
+// Enter feet 5
+// Enter inch 3
+// Feet: 5
+// Inch: 3
+// Feet: 5
+// Inch: 3
+// Feet: 10
+// Inch: 6
